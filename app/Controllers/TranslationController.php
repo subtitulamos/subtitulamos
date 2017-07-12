2930bd5a70376427151b909c1eeee11022e1c19a
Alejandro <athk@users.noreply.github.com>
2017-07-12 17:07:58 +0200

    Fix translation not working if multiple langs had been completed

--- app/Controllers/TranslationController.php
+++ app/Controllers/TranslationController.php
@@ -119,7 +119,7 @@ class TranslationController
         // Determine which secondary languages we can use
         $langRes = $em->createQuery("SELECT DISTINCT(s.lang) FROM App:Subtitle s WHERE s.version = :ver AND s.progress = 100")
             ->setParameter("ver", $sub->getVersion())
-            ->getOneOrNullResult();
+            ->getResult();
 
         $langs = [];
         foreach ($langRes as $lang) {
