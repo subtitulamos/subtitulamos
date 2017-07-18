21499f4d28a8e4b8d9aacd997ca03a5fdff04956
Alejandro <athk@users.noreply.github.com>
2017-07-18 23:06:52 +0200

    Add app:twig:clear-cache command

---
+++ app/Commands/ClearTwigCache.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is covered by the AGPLv3 license, which can be found at the LICENSE file in the root of this project.
+ * @copyright 2017 subtitulamos.tv
+ */
+
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Services\AssetManager as AssetManager;
+
+class ClearTwigCache extends Command
+{
+    protected function configure()
+    {
+        $this->setName('app:twig:clear-cache')
+            ->setDescription('Clears the temporary folders created by twig')
+            ->setHelp('Clears the files on the /tmp cache folder');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(__DIR__ . '/../../tmp', \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $fileinfo) {
+            $fn = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+            $fn($fileinfo->getRealPath());
+        }
+    }
+}
