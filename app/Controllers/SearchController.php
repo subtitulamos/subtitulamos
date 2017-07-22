48067ac129b00eec5a0decbd7f56dfd307dbd342
Alejandro <athk@users.noreply.github.com>
2017-07-22 03:43:23 +0200

    Do not show resyncs on "recent uploads"

--- app/Controllers/SearchController.php
+++ app/Controllers/SearchController.php
@@ -44,7 +44,7 @@ class SearchController
     public function listRecentUploads($request, $response, EntityManager $em, SlugifyInterface $slugify)
     {
         $page = max(1, min(10, (int)$request->getQueryParam('page', 1))) - 1;
-        $subs = $em->createQuery("SELECT s, v, e FROM App:Subtitle s JOIN s.version v JOIN v.episode e WHERE s.directUpload = 1 ORDER BY s.uploadTime DESC")
+        $subs = $em->createQuery("SELECT s, v, e FROM App:Subtitle s JOIN s.version v JOIN v.episode e WHERE s.directUpload = 1 AND s.resync = 0 ORDER BY s.uploadTime DESC")
             ->setMaxResults(10)
             ->setFirstResult($page * 10)
             ->getResult();
