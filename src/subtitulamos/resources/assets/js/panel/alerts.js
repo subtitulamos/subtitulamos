ca774e916198c2500558e91978e3d46c741cad9b
alex <athk@users.noreply.github.com>
2020-12-28 14:12:30 +0100

    Fix old alerts not uncollapsing

--- src/subtitulamos/resources/assets/js/panel/alerts.js
+++ src/subtitulamos/resources/assets/js/panel/alerts.js
@@ -1,12 +1,14 @@
-import '../../css/panel/panel_alerts.css';
+import "../../css/panel/panel_alerts.css";
 import { onDomReady } from "../utils";
 
 onDomReady(() => {
-  document.querySelector(".unhide-alert").addEventListener("click", function () {
-    let $cardBody = this.parentNode.parentNode.querySelector(".card-content");
-    let $icon = this.querySelector("i");
-    $cardBody.classList.toggle("hidden");
-    $icon.classList.toggle("fa-chevron-down");
-    $icon.classList.toggle("fa-chevron-up");
+  document.querySelectorAll(".unhide-alert").forEach(($alertCollapsible) => {
+    $alertCollapsible.addEventListener("click", function () {
+      let $cardBody = this.parentNode.parentNode.querySelector(".card-content");
+      let $icon = this.querySelector("i");
+      $cardBody.classList.toggle("hidden");
+      $icon.classList.toggle("fa-chevron-down");
+      $icon.classList.toggle("fa-chevron-up");
+    });
   });
 });
