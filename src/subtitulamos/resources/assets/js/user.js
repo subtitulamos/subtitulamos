a15c975566c348527614fd6215b288df8d3cdd20
alex <athk@users.noreply.github.com>
2020-12-28 20:18:27 +0100

    Add password reset functionality for mods

--- src/subtitulamos/resources/assets/js/user.js
+++ src/subtitulamos/resources/assets/js/user.js
@@ -3,20 +3,32 @@
  * @copyright 2020 subtitulamos.tv
  */
 
-import Vue from "vue";
 import "../css/user.scss";
+import { $getEle } from "./utils";
 
-let page = new Vue({
-  el: "#user-settings",
-  data: {
-    newpwd: "",
-  },
-});
+const $roleChangeForm = $getEle("#reset-user-pwd");
+if ($roleChangeForm) {
+  let manualSubmit = false;
+  $roleChangeForm.addEventListener("submit", function (e) {
+    if (manualSubmit) {
+      return;
+    }
 
-let page_2 = new Vue({
-  el: "#user-profile",
-  data: {
-    mode: "normal",
-    duration: "",
-  },
-});
+    e.preventDefault(); // Don't submit the form
+
+    Swal.fire({
+      type: "warning",
+      confirmButtonText: "Reiniciar",
+      cancelButtonText: "Cancelar",
+      showCancelButton: true,
+      html:
+        "Estás a punto de reiniciar la contraseña de este usuario. Se generará una nueva contraseña, y su contraseña actual dejará de valer para acceder." +
+        "<br/><br/>¿Estás seguro de querer continuar?",
+    }).then((result) => {
+      if (result.value) {
+        manualSubmit = true;
+        $roleChangeForm.submit();
+      }
+    });
+  });
+}
