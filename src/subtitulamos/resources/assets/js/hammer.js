d1dd0c87307bf09610d31c0f994d9d593f59ad2f
Francisco Moreno <50809863+fmoredesigns@users.noreply.github.com>
2020-12-30 00:27:19 +0100

    Style hammer page

--- src/subtitulamos/resources/assets/js/hammer.js
+++ src/subtitulamos/resources/assets/js/hammer.js
@@ -9,17 +9,23 @@ import { easyFetch } from "./utils";
 
 Vue.component("hammertarget", {
   template: `<div v-if='!deleted' class='grid-row'>
-        <div><a :class="userTypeClass" :href='\"/users/\"+id'>{{ username }}</a></div>
+        <div>
+          <a class="text small username" :class="userTypeClass" :href='\"/users/\"+id'>
+            <i v-if="isMod" class="fas fa-gem"></i>
+            <i v-else-if="isTT" class="fas fa-hand-sparkles"></i>
+            <span>{{ username }}</span>
+          </a>
+        </div>
         <div>
             <div>
               <span class="text bold">{{ total }}</span>
               <span class="text small">({{ corrected }} corregidas)</span>
             </div>
-            <a class="text tiny blue-a" href="javascript:void(0)" @click='completeHammer'>Borrar todas</a>
+            <a class="text tiny delete-button" href="javascript:void(0)" @click='completeHammer'>Borrar todas</a>
         </div>
         <div>
           <div class="text bold">{{ latest }}</div>
-          <a class="text tiny blue-a" v-show="latest > 0"href="javascript:void(0)" @click='latestHammer'>Borrar</a>
+          <a class="text tiny delete-button" v-show="latest > 0"href="javascript:void(0)" @click='latestHammer'>Borrar</a>
         </div>
     </div>`,
   props: ["id", "username", "userRoles", "countCorrected", "countLatest"],
@@ -28,6 +34,8 @@ Vue.component("hammertarget", {
       corrected: this.countCorrected * 1,
       latest: this.countLatest * 1,
       deleted: false,
+      isMod: this.userRoles.includes("ROLE_MOD"),
+      isTT: this.userRoles.includes("ROLE_TT"),
     };
   },
   computed: {
@@ -35,11 +43,9 @@ Vue.component("hammertarget", {
       return this.latest + this.corrected;
     },
     userTypeClass: function () {
-      let isTT = this.userRoles.includes("ROLE_TT");
-      let isMod = this.userRoles.includes("ROLE_MOD");
       return {
-        "role-tt": isTT && !isMod,
-        "role-mod": isMod,
+        "role-tt": this.isTT && !this.isMod,
+        "role-mod": this.isMod,
       };
     },
   },
