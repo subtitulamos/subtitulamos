d8181f8ed11b36b2636670d408a191d7d1a54fe1
Francisco Moreno <50809863+fmoredesigns@users.noreply.github.com>
2020-12-31 14:45:30 +0100

    Style alerts page

--- src/subtitulamos/resources/assets/js/panel/alerts.js
+++ src/subtitulamos/resources/assets/js/panel/alerts.js
@@ -1,11 +1,11 @@
 import { onDomReady } from "../utils";
 
 onDomReady(() => {
-  document.querySelectorAll(".unhide-alert").forEach(($alertCollapsible) => {
+  document.querySelectorAll(".card-header").forEach(($alertCollapsible) => {
     $alertCollapsible.addEventListener("click", function () {
-      let $cardBody = this.parentNode.parentNode.querySelector(".card-content");
-      let $icon = this.querySelector("i");
-      $cardBody.classList.toggle("hidden");
+      let $cardBody = this.parentNode.querySelector(".card-content");
+      let $icon = this.querySelector(".dropdown");
+      $cardBody.classList.toggle("expanded");
       $icon.classList.toggle("fa-chevron-down");
       $icon.classList.toggle("fa-chevron-up");
     });
