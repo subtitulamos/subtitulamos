0e5fbcbfeee1d8b413fbf77185d18077afe6946f
alex <athk@users.noreply.github.com>
2021-01-02 02:22:28 +0100

    Show previously banned users

--- src/subtitulamos/app/Controllers/Panel/PanelBanlistController.php
+++ src/subtitulamos/app/Controllers/Panel/PanelBanlistController.php
@@ -32,11 +32,32 @@ class PanelBanlistController
         return $banned_users;
     }
 
+    private function fetchOldBans($em)
+    {
+        $bans = $em->createQuery('SELECT ban FROM App:User u JOIN App:Ban ban WHERE u.id = ban.targetUser AND (ban.until < :right_now OR ban.unbanUser IS NOT NULL) ORDER BY u.username ASC')
+            ->setParameter('right_now', new \DateTime())
+            ->getResult();
+
+        $banned_users = [];
+        foreach ($bans as $ban) {
+            $banned_users[] = [
+                'by' => $ban->getByUser(),
+                'lifter' => $ban->getUnbanUser(),
+                'target' => $ban->getTargetUser(),
+                'until' => $ban->getUntil(),
+                'reason' => $ban->getReason()
+            ];
+        }
+
+        return $banned_users;
+    }
+
     public function view($request, $response, EntityManager $em, Twig $twig)
     {
         $banned_users = [
             'temporary' => $this->fetchBannedUsersByDate($em, new \DateTime('2037-01-01'), new \DateTime()),
-            'permanent' => $this->fetchBannedUsersByDate($em, new \DateTime('3000-01-01'), new \DateTime('2037-01-01'))
+            'permanent' => $this->fetchBannedUsersByDate($em, new \DateTime('3000-01-01'), new \DateTime('2037-01-01')),
+            'old' => $this->fetchOldBans($em)
         ];
 
         return $twig->render($response, 'panel/panel_banlist.twig', [
