5fc10da08e9291116fd944a96c875abc058666dc
alex <athk@users.noreply.github.com>
2021-01-03 22:53:50 +0100

    Fix CSS in disclaimer page

---
+++ src/subtitulamos/resources/assets/js/disclaimer.js
@@ -0,0 +1,9 @@
+/**
+ * This file is covered by the AGPLv3 license, which can be found at the LICENSE file in the root of this project.
+ * @copyright 2021 subtitulamos.tv
+ */
+
+/**
+ * This file exists because the CSS processed by webpack needs to exist in a file first
+ */
+import "../css/disclaimer.css";
