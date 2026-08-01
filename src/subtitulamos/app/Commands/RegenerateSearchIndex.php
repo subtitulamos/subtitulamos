9d4c231f59223921dd3522a52934c475d1e8613e
alex <athk@users.noreply.github.com>
2026-08-01 17:07:04 +0200

    Fix meili save

--- src/subtitulamos/app/Commands/RegenerateSearchIndex.php
+++ src/subtitulamos/app/Commands/RegenerateSearchIndex.php
@@ -21,10 +21,12 @@ class RegenerateSearchIndex extends Command
       ->setHelp('Renegerate the search index for all shows');
   }
 
-  protected function execute(InputInterface $input, OutputInterface $output, \MeiliSearch\Client $meili)
+  protected function execute(InputInterface $input, OutputInterface $output)
   {
     global $entityManager;
     $shows = $entityManager->getRepository('App:Show')->findAll();
+
+    $meili = Meili::getClient();
 
     // Make sure we clear the shows index, so we start from scratch
     $indexes = $meili->getIndexes();
